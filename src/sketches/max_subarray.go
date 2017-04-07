5b41d22c21f53f749570d618898a90beba4be45c
Alexander Kislitsky <akislitsky@mirantis.com>
2017-04-07 16:58:30 +0300

    Finding max subarray implemented

--- src/sketches/max_subarray.go
+++ src/sketches/max_subarray.go
@@ -15,11 +15,10 @@ func FindMaxCrossingArray(arr []int, low, mid, high int) ArrayRangeSum {
 	var leftIdx int
 	for idx := mid; idx >= low; idx-- {
 		leftSum += arr[idx]
-		if left > leftSum {
-			break
+		if leftSum > left {
+			leftIdx = idx
+			left = leftSum
 		}
-		leftIdx = idx
-		left = leftSum
 	}
 
 	var right int = math.MinInt64
@@ -28,12 +27,37 @@ func FindMaxCrossingArray(arr []int, low, mid, high int) ArrayRangeSum {
 
 	for idx := mid + 1; idx <= high; idx++ {
 		rightSum += arr[idx]
-		if right > rightSum {
-			break
+		if rightSum > right {
+			rightIdx = idx
+			right = rightSum
 		}
-		rightIdx = idx
-		right = rightSum
 	}
 
 	return ArrayRangeSum{low: leftIdx, high: rightIdx, sum: left + right}
+}
+
+func FindMaxSubArray(arr []int) ArrayRangeSum {
+	if len(arr) > 0 {
+		return FindMaxSubArrayRange(arr, 0, len(arr) - 1)
+	} else {
+		return ArrayRangeSum{}
+	}
+}
+
+func FindMaxSubArrayRange(arr []int, low, high int) ArrayRangeSum {
+	if low == high {
+		return ArrayRangeSum{low, high, arr[low]}
+	}
+
+	mid := (high + low) / 2
+	leftRange := FindMaxSubArrayRange(arr, low, mid)
+	rightRange := FindMaxSubArrayRange(arr, mid + 1, high)
+	crossRange := FindMaxCrossingArray(arr, low, mid, high)
+	if leftRange.sum >= rightRange.sum && leftRange.sum >= crossRange.sum {
+		return leftRange
+	} else if rightRange.sum >= leftRange.sum && rightRange.sum >= crossRange.sum {
+		return rightRange
+	} else {
+		return crossRange
+	}
 }
--- src/sketches/max_subarray_test.go
+++ src/sketches/max_subarray_test.go
@@ -1,7 +1,11 @@
 package sketches
 
 import (
+	"reflect"
 	"testing"
+	"math/rand"
+	"fmt"
+	"time"
 )
 
 func TestFindMaxCrossingArray(t *testing.T) {
@@ -55,3 +59,119 @@ func TestFindMaxCrossingArray(t *testing.T) {
 	}
 }
 
+func TestFindMaxSubarray(t *testing.T) {
+	var tests = []struct {
+		input []int
+		expected ArrayRangeSum
+	}{
+		// empty array
+		{
+			input: []int {},
+			expected: ArrayRangeSum{low: 0, high: 0, sum: 0},
+		},
+		// one element array
+		{
+			input: []int {1},
+			expected: ArrayRangeSum{low: 0, high: 0, sum: 1},
+		},
+		{
+			input: []int {-1},
+			expected: ArrayRangeSum{low: 0, high: 0, sum: -1},
+		},
+		{
+			input: []int {10},
+			expected: ArrayRangeSum{low: 0, high: 0, sum: 10},
+		},
+		// positive elements
+		{
+			input: []int {1, 10, 2},
+			expected: ArrayRangeSum{low: 0, high: 2, sum: 13},
+		},
+		{
+			input: []int {1, 10, 2, 0},
+			expected: ArrayRangeSum{low: 0, high: 2, sum: 13},
+		},
+		{
+			input: []int {1, 10, 2, 0, 0, 4},
+			expected: ArrayRangeSum{low: 0, high: 5, sum: 17},
+		},
+		{
+			input: []int {0, 1, 10, 2},
+			expected: ArrayRangeSum{low: 1, high: 3, sum: 13},
+		},
+		{
+			input: []int {5, 0, 1, 10, 2},
+			expected: ArrayRangeSum{low: 0, high: 4, sum: 18},
+		},
+		// negative elements
+		{
+			input: []int {-1, -10, -2},
+			expected: ArrayRangeSum{low: 0, high: 0, sum: -1},
+		},
+		{
+			input: []int {-1, -10, 0, -2},
+			expected: ArrayRangeSum{low: 2, high: 2, sum: 0},
+		},
+		{
+			input: []int {0, -1, -10, -2},
+			expected: ArrayRangeSum{low: 0, high: 0, sum: 0},
+		},
+		{
+			input: []int {-1, -10, -2, 0},
+			expected: ArrayRangeSum{low: 3, high: 3, sum: 0},
+		},
+	}
+
+	for _, c := range tests {
+		actual := FindMaxSubArray(c.input)
+		if c.expected != actual {
+			t.Errorf("Expected: %v, actual: %v",
+				c.expected, actual)
+		}
+	}
+
+}
+
+func TestFindMaxSubArrayReferenceValue(t *testing.T) {
+	var arr = []int {
+		13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7,
+	}
+	expectedRange := ArrayRangeSum{low: 7, high: 10, sum: 43}
+	actualRange := FindMaxSubArray(arr)
+	if expectedRange != actualRange {
+		t.Errorf("Expected range: %v, actual: %v",
+			expectedRange, actualRange)
+	}
+	//fmt.Println(actualRange)
+	expectedArray := []int {18, 20, -7, 12}
+	actualArray := arr[actualRange.low:actualRange.high + 1]
+	if !reflect.DeepEqual(expectedArray, actualArray) {
+		t.Errorf("Expected array: %v, actual: %v",
+			expectedArray, actualArray)
+	}
+}
+
+/*
+func TimeIt(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s tooks %s", name, elapsed)
+	fmt.Println()
+}
+
+func TestFindMaxSubArraySpeed(t *testing.T) {
+	min, max, length := -100, 100, 100000000
+	arr := make([]int, length)
+	rand.Seed(time.Now().Unix())
+	for i := 0; i < length; i++ {
+		arr[i] = rand.Intn(max - min) + min
+	}
+
+	name := fmt.Sprintf("finding max subarray in %d", len(arr))
+	defer TimeIt(time.Now(), name)
+	result := FindMaxSubArray(arr)
+
+	fmt.Printf("Found max subarray: %v, subarray len: %d",
+		result, result.high - result.low)
+	fmt.Println()
+}
+*/

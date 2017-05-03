bc06146c519190f8a8e12cc216ef955edbf5eb94
Alexander Kislitsky <sand8080@gmail.com>
2017-05-03 18:03:16 +0300

    Added benchmark test for Union operation

---
+++ src/github.com/sand8080/search/disjoint_sets_bench_test.go
@@ -0,0 +1,28 @@
+package search
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func random(min, max int) int {
+	if min == max {
+		return max
+	}
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max - min) + min
+}
+
+func BenchmarkDisjointSetInt_Union(b *testing.B) {
+	b.Logf("b.N is: %d\n", b.N)
+	ds := NewDisjointSetInt(b.N / 3)
+	for i := 0; i < b.N; i++ {
+		size := random(0, 4)
+		ids := make([]int, size)
+		for j := 0; j < size; j++ {
+			ids[j] = random(1, b.N)
+		}
+		ds.Union(ids)
+	}
+}

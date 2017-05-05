4fab49e80b60e15900f73d03e72dc8619b210bc1
Alexander Kislitsky <sand8080@gmail.com>
2017-05-05 15:50:19 +0300

    Bench test for groups emission added
    Test for random function added

--- src/github.com/sand8080/search/disjoint_sets_bench_test.go
+++ src/github.com/sand8080/search/disjoint_sets_bench_test.go
@@ -14,6 +14,35 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func TestRandomFailure(t *testing.T) {
+	defer func() {
+		if err := recover(); err == nil {
+			t.Error("Panic should be initiated on wrong params " +
+				"for random function")
+		}
+	}()
+	random(5, 1)
+}
+
+func TestRandom(t *testing.T) {
+	cases := []struct {
+		min, max int
+	}{
+		{min: 1, max: 1},
+		{min: 0, max: 5},
+		{min: -5, max: 0},
+		{min: 10, max: 20},
+		{min: -10, max: 10},
+	}
+	for _, c := range cases {
+		result := random(c.min, c.max)
+		if result < c.min || (result >= c.max && c.min != c.max) {
+			t.Errorf("Random value %d is out of bounds: '%d' - '%d'",
+				result, c.min, c.max)
+		}
+	}
+}
+
 func BenchmarkDisjointSetInt_Union(b *testing.B) {
 	ds := NewDisjointSetInt(b.N)
 	for i := 0; i < b.N; i++ {
@@ -23,5 +52,19 @@ func BenchmarkDisjointSetInt_Union(b *testing.B) {
 			ids[j] = random(1, b.N)
 		}
 		ds.Union(ids)
+	}
+}
+
+func BenchmarkDisjointSetInt_EmitGroups(b *testing.B) {
+	ds := NewDisjointSetInt(b.N)
+	for i := 0; i < b.N; i++ {
+		size := random(0, 4)
+		ids := make([]int, size)
+		for j := 0; j < size; j++ {
+			ids[j] = random(1, b.N)
+		}
+		ds.Union(ids)
+	}
+	for range ds.EmitGroups() {
 	}
 }
